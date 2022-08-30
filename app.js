9a1cad5a70070c6075eb593165d7e7d94e9bc91c
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2022-08-30 13:06:13 -0400

    Hey

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
+    console.log('github connecting checking...')
 });
 
 app.set('view engine', 'ejs');
