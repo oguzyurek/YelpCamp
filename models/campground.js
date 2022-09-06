9b1381c0d093701eaa7a7f3121e13f9fcf4643aa
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2022-09-05 22:07:16 -0400

    update

--- models/campground.js
+++ models/campground.js
@@ -6,13 +6,23 @@ const CampgroundSchema = new Schema({
         type: String,
         required: [true, 'name can not be blank']
     },
-    image: String,
+    image: {
+        type: String,
+        required: true
+    },
     price: {
         type: Number,
         min: 0,
+        required: true
     },
-    description: String,
-    location: String
+    description: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
