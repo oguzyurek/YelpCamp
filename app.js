b2be6724cd5e949cbd07bd8a072f8b4bf4850d16
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2022-09-25 19:57:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -42,12 +42,27 @@ app.use(morgan('dev'))
 app.use(session(sessionOption));
 app.use(flash())
 
+
+
 app.use((req, res, next) => {
     req.requestTime = Date.now();
     console.log(`req time is: ${req.requestTime}`)
     console.log(req.method, req.path);
     return next();
-})
+});
+
+/////ROUTES/////ROUTES/////ROUTES/////ROUTES/////ROUTES/////ROUTES////
+
+app.use('/campgrounds', campgrounds)
+
+
+
+
+
+
+/////ROUTES/////ROUTES/////ROUTES/////ROUTES/////ROUTES/////ROUTES////
+
+
 
 /////FLASH/////FLASH/////FLASH/////FLASH/////FLASH/////FLASH/////FLASH
 
@@ -57,17 +72,13 @@ app.use((req, res, next) => {
 })
 
 
-
-
-
-
-
-
-
-
-
 /////FLASH/////FLASH/////FLASH/////FLASH/////FLASH/////FLASH/////FLASH
 
+
+
+
+
+/////FUNCTIONS/////FUNCTIONS/////FUNCTIONS/////FUNCTIONS/////FUNCTIONS
 
 const verifyPassword = (req, res, next) => {
     const { password } = req.query
@@ -87,62 +98,13 @@ const validateCampground = (req, res, next) => {
     }
 }
 
-app.get('/', (req, res) => {
-    res.render('home')
-});
-
 function wrapAsync(fn) {
     return function (req, res, next) {
         fn(req, res, next).catch(e => next(e))
     }
 }
 
-app.get('/campgrounds', wrapAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
-}));
-
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-})
-
-app.post('/campgrounds', validateCampground, wrapAsync(async (req, res, next) => {
-    console.log(result);
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.get('/campgrounds/:id', wrapAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
-    if (!campground) {
-        return next(new AppError('Camground Not Found', 404))
-    }
-
-    res.render('campgrounds/show', { campground })
-}));
-
-app.get('/campgrounds/:id/edit', wrapAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', { campground });
-}));
-
-app.get('/secret', verifyPassword, (req, res) => {
-    res.send('This is my secret.')
-})
-
-app.put('/campgrounds/:id', validateCampground, wrapAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`)
-}));
-
-app.delete('/campgrounds/:id', wrapAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const deleted = await Campground.findByIdAndDelete(id);
-    console.log(`${deleted.title} ${deleted.location}  is deleted.`)
-    res.redirect('/campgrounds');
-}))
+/////FUNCTIONS/////FUNCTIONS/////FUNCTIONS/////FUNCTIONS/////FUNCTIONS
 
 //////REVIEWS////////////REVIEWS////////////REVIEWS////////////
 
