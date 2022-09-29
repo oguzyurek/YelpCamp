363f4df5937496bf7db3e34f622fb10fb6ad6a69
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2022-09-29 17:26:49 -0400

    u

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const sessionOption = {
     resave: false,
     saveUninitialized: true,
     cookie: {
+        httpOnly: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
 
