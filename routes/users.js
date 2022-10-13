39501b17c93abff83927d22c04fd79491117f1ab
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2022-10-13 16:48:39 -0400

    add cacthAsync to user.js

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utils/cacthAsync');
 const User = require('../models/user');
 
 router.get('/register', async (req, res) => {
     res.render('../views/users/register')
 })
-router.post('/register', async (req, res) => {
+router.post('/register', cacthAsync(async (req, res) => {
     const { username, password, email } = req.body;
     const user = new User({ email, username });
     const registeredUser = await User.register(user, password);
     console.log(registeredUser);
     req.flash('success', 'You successfully registered.')
     res.redirect('/campgrounds');
-})
+}))
 router.get('/signin', async (req, res) => {
     res.render('../views/users/signin')
 })
