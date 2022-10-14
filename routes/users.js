d3102337ab3c95f0443fcad2f910a9518b5b72e6
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2022-10-13 20:37:17 -0400

    add signin

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const cacthAsync = require('../utils/cacthAsync');
 const User = require('../models/user');
+
 
 router.get('/register', async (req, res) => {
     res.render('../views/users/register')
@@ -23,9 +25,9 @@ router.post('/register', cacthAsync(async (req, res) => {
 router.get('/signin', async (req, res) => {
     res.render('../views/users/signin')
 })
-router.post('/signin', async (req, res) => {
-    res.send(req.body);
-    console.log
+router.post('/signin', passport.authenticate('local', { failureFlash: true, failureRedirect: '/signin' }), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
 })
 
 
