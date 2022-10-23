c4d31ac922116c2b45234ccb3a6c02ff2a7f02be
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2022-10-23 10:50:47 -0400

    update users routes

--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,14 @@ const cacthAsync = require('../utils/cacthAsync');
 const users = require('../controllers/users')
 const User = require('../models/user');
 
+router.route('/register')
+    .get(users.renderRegisterPage)
+    .post(cacthAsync(users.postRegisterPage))
 
-router.get('/register', users.renderRegisterPage)
-
-router.post('/register', cacthAsync(users.postRegisterPage))
-
-router.get('/signin', users.renderSignin)
-
+router.route('/signin')
+    .get(users.renderSignin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/signin', keepSessionInfo: true, }), users.validateSignin)
 //keepSessionInfo has to be true to redirect after signin where we were.
-router.post('/signin', passport.authenticate('local', { failureFlash: true, failureRedirect: '/signin', keepSessionInfo: true, }), users.validateSignin)
 
 router.get('/logout', users.logout);
 
