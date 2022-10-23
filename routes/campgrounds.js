d5cb831d5859c4629f51e00ddb23d8f1a1286156
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2022-10-23 10:48:13 -0400

    update campgrounds routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,20 +11,18 @@ const campground = require('../models/campground');
 const campgrounds = require('../controllers/campgrounds');
 const cacthAsync = require('../utils/cacthAsync.js');
 
+router.route('/')
+    .get(cacthAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, cacthAsync(campgrounds.postNewCampground));
 
-router.get('/', cacthAsync(campgrounds.index));
+router.route('/:id')
+    .get(cacthAsync(campgrounds.renderCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, cacthAsync(campgrounds.postEditPage))
+    .delete(isLoggedIn, isAuthor, cacthAsync(campgrounds.deleteCampground))
 
 router.get('/new', isLoggedIn, campgrounds.renderNewPage);
 
-router.post('/', isLoggedIn, validateCampground, cacthAsync(campgrounds.postNewCampground));
-
-router.get('/:id', cacthAsync(campgrounds.renderCampground));
-
 router.get('/:id/edit', isAuthor, isLoggedIn, cacthAsync(campgrounds.renderEditPage));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, cacthAsync(campgrounds.postEditPage));
-
-router.delete('/:id', isLoggedIn, isAuthor, cacthAsync(campgrounds.deleteCampground));
 
 module.exports = router;
 
