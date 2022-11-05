cbe1fb58dab2e9f6f5d8c54bf60192a865c473fb
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2022-11-05 15:10:09 -0400

    update campground controller

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -11,6 +11,7 @@ module.exports.renderNewPage = (req, res) => {
 
 module.exports.postNewCampground = async (req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.image = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'New campground successfully created.');
