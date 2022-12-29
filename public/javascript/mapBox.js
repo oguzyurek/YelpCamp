a6924b19d5da497b6674fcf422a25afdc2a1a345
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2022-12-28 20:17:56 -0500

    update mapbox

--- public/javascript/mapBox.js
+++
@@ -1,8 +0,0 @@
-
-mapboxgl.accessToken = mapToken;
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-73.935242, 40.730610], // starting position [lng, lat]
-    zoom: 9, // starting zoom
-});
