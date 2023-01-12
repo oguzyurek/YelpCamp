578da366c214f9be35f51076ee33afc559e0fb41
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2023-01-11 21:05:41 -0500

    Update index.js

--- seeds/index.js
+++ seeds/index.js
@@ -20,7 +20,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i <= 30; i++) {
+  for (let i = 0; i <= 500; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
